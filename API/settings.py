1a2cecc60d31553f6e121471cbc1d2a18f565fb2
Vukknesh <leomcn@hotmail.com>
2019-04-18 00:12:38 -0300

    media

--- API/settings.py
+++ API/settings.py
@@ -160,6 +160,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CORS_ORIGIN_ALLOW_ALL = True
 
